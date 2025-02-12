ca2f735355db953070c003c741f8f9599944c670
splint <splint@pop-os.lan>
2025-02-12 11:18:25 -0800

    Cleanup

--- src/bt_node.cpp
+++ src/bt_node.cpp
@@ -1,48 +1,49 @@
 #include <behaviortree_cpp_v3/bt_factory.h>
 #include <rclcpp/rclcpp.hpp>
 
-class SayHello : public BT::SyncActionNode
-{
+
+class SayHello : public BT::SyncActionNode {
+
 public:
-    SayHello(const std::string& name, const BT::NodeConfiguration& config)
-	: BT::SyncActionNode(name, config) {}
 
-    static BT::PortsList providedPorts()
-    {
-	return { };
-    }
+  SayHello(const std::string& name, const BT::NodeConfiguration& config)
+    : BT::SyncActionNode(name, config) {}
 
-    BT::NodeStatus tick() override
-    {
-	RCLCPP_INFO(rclcpp::get_logger("SayHello"), "Hello, World!");
-	return BT::NodeStatus::SUCCESS;
-    }
+  static BT::PortsList providedPorts() {
+    return { };
+  }
+
+  BT::NodeStatus tick() override {
+    RCLCPP_INFO(rclcpp::get_logger("SayHello"), "Hello, World!");
+    return BT::NodeStatus::SUCCESS;
+  }
+
 };
 
-int main(int argc, char** argv)
-{
-    rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("bt_node");
 
-    BT::BehaviorTreeFactory factory;
-    factory.registerNodeType<SayHello>("SayHello");
+int main(int argc, char** argv) {
 
-    auto tree = factory.createTreeFromText(R"(
-	<root main_tree_to_execute="MainTree">
-	    <BehaviorTree ID="MainTree">
-		<SayHello/>
-	    </BehaviorTree>
-	</root>
-    )");
+  rclcpp::init(argc, argv);
 
-    while (rclcpp::ok())
-    {
-	tree.tickRoot();
-	rclcpp::spin_some(node);
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
+  auto node = rclcpp::Node::make_shared("bt_node");
 
-    rclcpp::shutdown();
-    return 0;
+  BT::BehaviorTreeFactory factory;
+  factory.registerNodeType<SayHello>("SayHello");
+
+  auto tree = factory.createTreeFromText(R"(
+    <root main_tree_to_execute="MainTree">
+	  <BehaviorTree ID="MainTree">
+        <SayHello/>
+	  </BehaviorTree>
+    </root>
+  )");
+
+  while (rclcpp::ok()) {
+    tree.tickRoot();
+    rclcpp::spin_some(node);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  rclcpp::shutdown();
+  return 0;
 }
-
